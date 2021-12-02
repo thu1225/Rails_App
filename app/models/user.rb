3e0c1833e8be2ceaca610975846fef2f6b4da5d6
thu1225 <atpang25@gmail.com>
2021-12-02 14:25:10 +0700

    Validation User sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
-
-
-
+    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "phải nhập đúng định dạng email (xxx@gmail.com)"}
+    validates :password, :presence => true, :confirmation => true, length: { minimum: 6}
+    validates :sdt, presence: true,length: { is: 9 }, format: { with: /\d[0-9]\)*\z/, message: "phải là ký tự số "}
 end
